672a1b8d6eebd7a8efde8f0b13f879f1de85949f
Binsquare <hebinsquared@gmail.com>
2018-10-21 11:43:22 -0500

    established mailgun api, implmemented accounts & account routing.

--- server/main.js
+++ server/main.js
@@ -1,5 +1,18 @@
 import { Meteor } from 'meteor/meteor';
+import { Email } from 'meteor/email'
+import { Mongo } from 'meteor/mongo';     
+import { Accounts } from 'meteor/std:accounts-ui'
+
+
+if (Meteor.isServer) {
+
+  Meteor.publish('allUsers', function () {
+    return Meteor.users.find({});  
+  });    
+
+}
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  process.env.MAIL_URL = 
+  "smtp://postmaster@sandboxc94b0dcf634c4addbd9b62c773b49f77.mailgun.org:52e3e3667a877ffed019cd420f4eb66c@smtp.mailgun.org:587";
 });
